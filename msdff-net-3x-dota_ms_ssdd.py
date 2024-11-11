7cd6475df4cec5187fa21eeadd3e94c6acb88135
zhongzhen sun <56304912+SZZ-SXM@users.noreply.github.com>
2024-11-11 19:28:28 +0800

    Add files via upload

---
+++ msdff-net-3x-dota_ms_ssdd.py
@@ -0,0 +1,79 @@
+_base_ = [
+    './_base_/default_runtime_ssdd.py', './_base_/schedule_3x_ssdd.py',
+    './_base_/dota_rr_ssdd.py'
+]
+checkpoint = r''  # noqa
+
+angle_version = 'le90'
+model = dict(
+    type='mmdet.MSDFF_Net',
+    data_preprocessor=dict(
+        type='mmdet.DetDataPreprocessor',
+        mean=[103.53, 116.28, 123.675],
+        std=[57.375, 57.12, 58.395],
+        bgr_to_rgb=False,
+        boxtype2tensor=False,
+        batch_augments=None),
+    backbone=dict(
+        type='mmdet.MSLK_Net',
+        arch='P5',
+        expand_ratio=0.5,
+        deepen_factor=1,
+        widen_factor=1,
+        channel_attention=True,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU'),
+        init_cfg=dict(
+            type='Pretrained', prefix='backbone.', checkpoint=checkpoint)),
+    neck=dict(
+        type='mmdet.DFF_Net',
+        in_channels=[256, 512, 1024],
+        out_channels=256,
+        num_csp_blocks=3,
+        expand_ratio=0.5,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU')),
+    bbox_head=dict(
+        type='RotatedMSDFFNetSepBNHead',
+        num_classes=1,  # 类别数
+        in_channels=256,
+        stacked_convs=2,
+        feat_channels=256,
+        angle_version=angle_version,
+        anchor_generator=dict(
+            type='mmdet.MlvlPointGenerator', offset=0, strides=[8, 16, 32]),
+        bbox_coder=dict(
+            type='DistanceAnglePointCoder', angle_version=angle_version),
+        loss_cls=dict(
+            type='mmdet.QualityFocalLoss',
+            use_sigmoid=True,
+            beta=2.0,
+            loss_weight=1.0),
+        loss_bbox=dict(type='RotatedGPDLoss', mode='linear', loss_weight=2.0),
+        with_objectness=False,
+        exp_on_reg=True,
+        share_conv=True,
+        pred_kernel_size=1,
+        use_hbbox_loss=False,
+        scale_angle=False,
+        loss_angle=None,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU')),
+    train_cfg=dict(
+        assigner=dict(
+            type='mmdet.DynamicSoftLabelAssigner',
+            iou_calculator=dict(type='RBboxOverlaps2D'),
+            topk=13),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=2000,
+        min_bbox_size=0,
+        score_thr=0.3, # 置信度
+        nms=dict(type='nms_rotated', iou_threshold=0.1),
+        max_per_img=2000),
+)
+
+# batch_size = (2 GPUs) x (4 samples per GPU) = 8
+train_dataloader = dict(batch_size=4, num_workers=1)
